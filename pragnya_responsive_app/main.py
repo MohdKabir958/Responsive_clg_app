d09fe82f5033781644d29d4d3dae6b1a27b65ab4
md ehtesham <ehteshammd089@gmail.com>
2024-12-19 08:04:03 +0530

    added options

--- pragnya_responsive_app/main.py
+++ pragnya_responsive_app/main.py
@@ -212,3 +212,7 @@ def display_image(filename):
     return send_from_directory(upload_folder, filename)
 
 
+@main.route('/default_img')
+def get_default_img():
+    default_img_path = 'static/images_folder/default-img.png'
+    return redirect(url_for('static', filename='images_folder/default-img.png'))
